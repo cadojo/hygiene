1e7307d3d5a160ad46b4b8f373eab769feadb9dd
Joe Carpinelli <jdcarpinelli@gmail.com>
2022-03-18 23:31:24 -0500

    Initial commit for hygiene!

--- src/hygiene/__init__.py
+++ src/hygiene/__init__.py
@@ -1,16 +1,22 @@
-import sys
+"""Functions to help with module hygiene!
 
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+The main function exported is `cleanup`. The `cleanup`
+function generates code which, upon execution, will
+`del` every `local` variable that is not in some 
+pre-defined list of `export` variables.
+"""
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = version(dist_name)
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
-finally:
-    del version, PackageNotFoundError
+def cleanup(export: str = "__export__") -> str:
+    """ Generate code to clean up the current namespace!
+    
+    Usage: `exec(cleanup())`
+    """
+    from textwrap import dedent
+    return dedent(
+        f"""
+        for variable in locals().copy():
+            if variable != "{export}" and variable not in {export}:
+                del locals()[variable]
+        del variable, {export}
+        """
+    )
